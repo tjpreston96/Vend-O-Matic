b047fe9c0fc5dc819c71bf15f491673ffa93f38a
Tyler Preston <tpreston96@gmail.com>
2022-08-21 01:00:38 -0500

    stub routes_list view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+
+from .utils import get_routes
 
 # Create your views here.
+
+
+@api_view(["GET"])
+def routes_list(request):
+    return get_routes(request)
