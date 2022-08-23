9a4d44239a685860dcbabe926bd222a710a76ae8
Tyler Preston <tpreston96@gmail.com>
2022-08-23 17:19:56 -0500

    stub get_item view

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 
-from .utils import get_routes, add_coin, inventory_list, refund_coins
+from .utils import get_quantity, get_routes, add_coin, inventory_list, refund_coins
 
 # Create your views here.
 
@@ -21,3 +21,9 @@ def add_or_refund_coins(request):
 @api_view(["GET"])
 def get_inventory(request):
     return inventory_list(request)
+
+
+@api_view(["GET"])
+def get_item(request, pk):
+    if request.method == "GET":
+        return get_quantity(request, pk)
