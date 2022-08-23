082cb0b3e943dc098474e77a285b65f0d147302d
Tyler Preston <tpreston96@gmail.com>
2022-08-23 17:19:38 -0500

    stub get_item path

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("routes/", views.routes_list),
     path("", views.add_or_refund_coins, name="coins"),
-    path('inventory/', views.get_inventory)
+    path('inventory/', views.get_inventory),
+    path('inventory/<int:pk>/', views.get_item)
 ]
