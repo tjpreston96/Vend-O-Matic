fab32bb7e2d80a3efbe36d636f8331211a28ca96
Tyler Preston <tpreston96@gmail.com>
2022-08-23 15:18:30 -0500

    stub refund_coins

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 
-from .utils import get_routes, add_coin
+from .utils import get_routes, add_coin, refund_coins
 
 # Create your views here.
 
@@ -10,9 +10,9 @@ def routes_list(request):
     return get_routes(request)
 
 
-@api_view(["PUT"])
+@api_view(["PUT", "DELETE"])
 def add_or_refund_coins(request):
     if request.method == "PUT":
         return add_coin(request)
-    # if request.method == "DELETE":
-    #     return refund_coins(request)
+    if request.method == "DELETE":
+        return refund_coins(request)
