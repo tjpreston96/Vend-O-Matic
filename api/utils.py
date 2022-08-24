3c57a5402e358883811c2e84271d58d19394deda
Tyler Preston <tpreston96@gmail.com>
2022-08-24 18:02:05 -0500

    update routes_list

--- api/utils.py
+++ api/utils.py
@@ -3,31 +3,11 @@ from rest_framework import status
 from rest_framework.response import Response
 
 from .models import Currency, Item
-from .serializers import CurrencySerializer, ItemSerializer
-
-
-def get_routes(request):
-    routes = [
-        {
-            "Endpoint": "/",
-            "Method": "PUT",
-            "Body": {"coin": 1},
-            "Description": "Allows insertion of a single coin (US Quarter) into Vend-O-Matic. Response contains 204 status code and headers containing # of coins accepted.",
-        },
-        {
-            "Endpoint": "/",
-            "Method": "DELETE",
-            "Body": None,
-            "Description": "Vend-O-Matic returns all coins accepted during current transaction. Response contains 204 status code and headers containing # of coins returned.",
-        },
-    ]
-
-    return Response(routes)
 
 
 def landing(request):
     coins = Currency.objects.first().quantity
-    welcome = "Welcome to the Vend-O-Matic Browsable API. To view all possible actions navigate to ['/routes']"
+    welcome = "Welcome to the Vend-O-Matic Browsable API. To view all possible actions, please navigate to ['/routes']."
     return Response(welcome, status=status.HTTP_200_OK, headers={"X-Coins": coins})
 
 
@@ -90,6 +70,38 @@ def vend_item(request, pk):
         )
 
 
-# {
-#     "coin": 1
-# }
+def get_routes(request):
+    routes = [
+        {
+            "Endpoint": "",
+            "Method": "PUT",
+            "Body": {"coin": 1},
+            "Description": "Allows insertion of a single coin (US Quarter) into Vend-O-Matic. Response contains a 204 status code and headers containing # of coins accepted.",
+        },
+        {
+            "Endpoint": "",
+            "Method": "DELETE",
+            "Body": None,
+            "Description": "Vend-O-Matic returns all coins accepted during current transaction. Response contains a 204 status code and headers containing # of coins returned.",
+        },
+        {
+            "Endpoint": "inventory/",
+            "Method": "GET",
+            "Body": None,
+            "Description": "Vend-O-Matic returns entire remaining inventory. Response contains a 200 status code, and a response body containing a list of integers corresponding to item quanitities.",
+        },
+        {
+            "Endpoint": "inventory/:id",
+            "Method": "GET",
+            "Body": None,
+            "Description": "Vend-O-Matic returns item inventory. Response contains a 200 status code, a response body containing an integer corresponding to individual item quantity.",
+        },
+        {
+            "Endpoint": "inventory/:id",
+            "Method": "PUT",
+            "Body": None,
+            "Description": "Vend-O-Matic vends a beverage to customer. Response contains a 200 status code, a response body containing # of items vended, and headers containing # of coins returned and item inventory remaining.",
+        },
+    ]
+
+    return Response(routes)
