715036578677ffb87a068ce62b7cc8c5c96f22ce
Tyler Preston <tpreston96@gmail.com>
2022-08-23 21:02:59 -0500

    stub vend_item view

--- api/views.py
+++ api/views.py
@@ -1,6 +1,14 @@
 from rest_framework.decorators import api_view
 
-from .utils import get_quantity, get_routes, add_coin, inventory_list, refund_coins
+from .utils import (
+    add_coin,
+    get_item_quantity,
+    get_routes,
+    inventory_list,
+    landing,
+    refund_coins,
+    vend_item,
+)
 
 # Create your views here.
 
@@ -10,8 +18,10 @@ def routes_list(request):
     return get_routes(request)
 
 
-@api_view(["PUT", "DELETE"])
+@api_view(["GET","PUT", "DELETE"])
 def add_or_refund_coins(request):
+    if request.method == 'GET':
+        return landing(request)
     if request.method == "PUT":
         return add_coin(request)
     if request.method == "DELETE":
@@ -23,7 +33,9 @@ def get_inventory(request):
     return inventory_list(request)
 
 
-@api_view(["GET"])
+@api_view(["GET", "PUT"])
 def get_item(request, pk):
     if request.method == "GET":
-        return get_quantity(request, pk)
+        return get_item_quantity(request, pk)
+    if request.method == "PUT":
+        return vend_item(request, pk)
