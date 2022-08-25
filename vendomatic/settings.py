b9be39ecb978a1580c2498132a60aa2fbe515dd7
Tyler Preston <tpreston96@gmail.com>
2022-08-24 22:33:32 -0500

    deployment config

--- vendomatic/settings.py
+++ vendomatic/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATICFILES_STORAGE ='django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
