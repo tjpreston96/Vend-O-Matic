0d9012a2659e243ecb34bdf396ae7879ce69c8c2
Tyler Preston <tpreston96@gmail.com>
2022-08-24 22:19:26 -0500

    test

--- vendomatic/settings.py
+++ vendomatic/settings.py
@@ -38,14 +38,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "api",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # Added Apps
+    "api",
     "rest_framework",
 ]
 
@@ -130,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
