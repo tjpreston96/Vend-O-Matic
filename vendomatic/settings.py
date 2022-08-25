75c2d1c8e922aca1e5921ea880f5f55b6c8b770b
Tyler Preston <tpreston96@gmail.com>
2022-08-24 19:05:25 -0500

    static file storage

--- vendomatic/settings.py
+++ vendomatic/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
