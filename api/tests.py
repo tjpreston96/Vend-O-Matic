f2c50ef9a618bb549dd15f29e4269834edfaa621
Tyler Preston <tpreston96@gmail.com>
2022-09-06 04:23:50 -0500

    clean up tests and type hinting

--- api/tests.py
+++ api/tests.py
@@ -1,17 +1,19 @@
-from django.test import Client, RequestFactory, TestCase
+from django.test import TestCase
+
+# Extends default client class
+from rest_framework.test import APIClient
 
 from .models import Currency, Item
 from .views import *
 
-c = Client()
+client = APIClient()
 
 # Create your tests here.
 class CurrencyTestCase(TestCase):
-    def setup(self):
-        self.factory = RequestFactory()
+    def setUp(self) -> None:
         self.currency = Currency.objects.create(name="coin")
 
-    def test_setup(self):
+    def test_currency_creation(self) -> None:
         self.assertEqual(len(Currency.objects.all()), 1)
         self.assertEqual(self.currency.name, "coin")
         self.assertEqual(self.currency.quantity, 0)
