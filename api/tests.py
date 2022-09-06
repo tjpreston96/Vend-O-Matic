5f38579913b99f20cedeb8ca0c5405958e3a1ed2
Tyler Preston <tpreston96@gmail.com>
2022-09-06 03:39:43 -0500

    fix assertion

--- api/tests.py
+++ api/tests.py
@@ -12,6 +12,6 @@ class CurrencyTestCase(TestCase):
         self.currency = Currency.objects.create(name="coin")
 
     def test_setup(self):
-        self.assertEqual(len(Currency.objects.all(), 1))
+        self.assertEqual(len(Currency.objects.all()), 1)
         self.assertEqual(self.currency.name, "coin")
         self.assertEqual(self.currency.quantity, 0)
