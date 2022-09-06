c71a1a9942efbfcd7cafaa129da3a17604e849eb
Tyler Preston <tpreston96@gmail.com>
2022-09-05 23:53:59 -0500

    start Currency tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import Client, RequestFactory, TestCase
+
+from .models import Currency, Item
+from .views import *
+
+c = Client()
 
 # Create your tests here.
+class CurrencyTestCase(TestCase):
+    def setup(self):
+        self.factory = RequestFactory()
+        self.currency = Currency.objects.create(name="coin")
+
+    def test_setup(self):
+        self.assertEqual(len(Currency.objects.all(), 1))
+        self.assertEqual(self.currency.name, "coin")
+        self.assertEqual(self.currency.quantity, 0)
