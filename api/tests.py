f2d418c7c911fa8259c1107dc7983aa16d979dc7
Tyler Preston <tpreston96@gmail.com>
2022-09-06 05:26:51 -0500

    test_add_coin

--- api/tests.py
+++ api/tests.py
@@ -11,15 +11,27 @@ client = APIClient()
 # Create your tests here.
 class CurrencyTestCase(TestCase):
     def setUp(self) -> None:
+        # Currency.objects.create(name="coin")
         self.currency = Currency.objects.create(name="coin")
 
     def test_currency_creation(self) -> None:
+        currency = Currency.objects.first()
+
         self.assertEqual(len(Currency.objects.all()), 1)
-        self.assertEqual(self.currency.name, "coin")
-        self.assertEqual(self.currency.quantity, 0)
+        self.assertEqual(currency.name, "coin")
+        self.assertEqual(currency.quantity, 0)
 
     def test_landing(self) -> None:
         response = client.get("/")
+        currency = Currency.objects.first()
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(int(response.headers["X-Coins"]), self.currency.quantity)
+        self.assertEqual(int(response.headers["X-Coins"]), currency.quantity)
+
+    def test_add_coin(self) -> None:
+        response = client.put("/", {"coin": 1}, format="json")
+        currency = Currency.objects.first()
+
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(currency.quantity, 1)
+        
